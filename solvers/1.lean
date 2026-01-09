25cd03035689ae526148c11bbbb980446016006f
Ciro Santilli <cirosantilli@proton.me>
2026-01-09 08:17:29 +0000

    Lean setup

--- solvers/1.lean
+++ solvers/1.lean
@@ -12,7 +12,8 @@ example : p1sol 10 = 23 := rfl
 
 theorem p1_10 : p1sol 10 = 23 := by native_decide
 theorem p1 : p1sol 1000 = 233168 := by native_decide
-theorem p1_equiv (n : ℕ) : p1def n = p1sol n := sorry
+theorem p1_equiv (n : Nat) : p1def n = p1sol n := by
+  sorry
 
 def main : IO Unit := do
   IO.println (p1sol 1000)
